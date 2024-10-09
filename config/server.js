49abbfb3ea3ae00aacbc7e0f4cf9290ae5ce9730
kamrankhoxa <kamrankhoxa@outlook.com>
2024-10-09 23:59:45 +0500

    [update]: init backlogged.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6455aad034a64f765034a6f4727aeeb8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e3f201b4-6f76-4c77-91bc-dd1a65e6588d'
+};
